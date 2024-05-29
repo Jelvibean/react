204f95dde4093e4bd769b3e165d6510676cb34ad
Yelvy Buitrago <yelvyb@gmail.com>
2024-05-28 23:08:38 -0400

    YB: Ading form submission with state keeping all old posts.  Important to look over again.

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,17 +2,50 @@ import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 // both onBodyChange, onAuthorChange are functions passed to be triggered on the onChange events
-function NewPost({ onBodyChange, onAuthorChange, onCancel }) {
+function NewPost({ onCancel, onAddPost }) {
+  const [enteredBody, setEnteredBody] = useState("Welcome");
+  const [enteredAuthor, setEnteredAuthor] = useState("Who are you?");
+
+  // these guys set the state for both pieces when they are triggered from the NewPost Child
+  function BodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function onSubmitHandler(event) {
+    //this prevents browser default to send an http request.
+    event.preventDefault();
+
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+
+    onAddPost(postData);
+    //console.log(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    // onsubmit handler.
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={BodyChangeHandler} />
       </p>
 
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} />
+        <input
+          type="text"
+          id="name"
+          required
+          autoComplete="off"
+          onChange={authorChangeHandler}
+        />
       </p>
       <p className={classes.actions}>
         {/* by default a button in a form will submt.. so add type=button so this button doesnt  */}
