52bb5e455bc9fcb713018bcad2d558752e05b0d4
Jelvibean <yelvyb@yahoo.com>
2024-06-05 14:04:18 -0400

    YB:  adding all the pieces that make up the loader pieces. (#8)
    Co-authored-by: Yelvy Buitrago <yelvyb@gmail.com>

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 function Posts() {
   return (
     <>
+      {/* This out will bring in NewPost */}
       <Outlet />
       <main>
         <PostsList />
@@ -13,3 +14,9 @@ function Posts() {
 }
 
 export default Posts;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/posts");
+  const resData = await response.json();
+  return resData.posts;
+}
