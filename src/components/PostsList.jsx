c3a26adc2d5cf720d1e03c20a4c61108db902393
Yelvy Buitrago <yelvyb@gmail.com>
2024-06-09 22:47:48 -0400

    YB: FINAL PROJECT FOR REACT AREA.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,43 +5,17 @@ import { useLoaderData } from "react-router-dom";
 function PostsList() {
   const posts = useLoaderData();
 
-  function addPostHandler(postData) {
-    // Nice example of a POST request. Fetch sends data back.
-    // By default a fetch is set to a GET request. So add method to it.
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    // look into what spread operator does.
-    //This is not good because if update state and new state is based on previsou state value.
-    // you should actually pass a function to set post .  This function will be call automacticall by react when every you call setPost and this
-    //function will acutomatically receive current state and you will return a new state.
-
-    // setPosts([setPosts, ...posts]);
-
-    // Just remember if new state depends on old state.. use a function to update
-    // state and you get the old state automatically and you can return the new value.
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-
-    //consoling posts you can clearly see that every time you submit, the first time,
-    //posts is empty but after that every item entered has the previous post added.
-    // this is because of the spread operator.
-    // also you want to item that is submitted, its in postData.
-    //Not sure what existing post is. ***
-    console.log("I am posts", posts);
-    console.log("I am postData", postData);
-  }
-
   return (
-    // isPosting is the flag sent over to see if youshow modal or not.
     <>
       {posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post, index) => (
-            <Post key={index} author={post.author} body={post.body} />
+            <Post
+              key={index}
+              id={post.id}
+              author={post.author}
+              body={post.body}
+            />
           ))}
         </ul>
       )}
